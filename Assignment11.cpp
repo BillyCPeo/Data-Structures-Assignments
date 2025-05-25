6229dc4ac55c74a5633836ca2ea001ac6db8cc14
BillyCPeo <114188310+BillyCPeo@users.noreply.github.com>
2025-05-25 00:20:19 -0700

    Add files via upload

---
+++ Assignment11.cpp
@@ -0,0 +1,43 @@
+/*Billy Peo
+Data Structures
+Professor Wu*/
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+//Main
+int main() {
+    int arr[] = { 5, 2, 9, 1, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array (Insertion Sort): ";
+    printArray(arr, n);
+
+    return 0;
+}
